5cc54374fff01412ce74d5aa249883acb6b70d34
Jhonatan Bianchi <jhonatanbianchi@gmail.com>
2020-05-02 17:35:44 -0300

    cors

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -49,7 +49,14 @@ class BaseController extends ActiveController
         $behaviors['corsFilter'] = [
             'class' => Cors::className(),
             'cors' => [
-                'Access-Control-Expose-Headers' => ['X-Pagination-Total-Count','X-Pagination-Page-Count', 'X-Pagination-Current-Page', 'X-Pagination-Per-Page'],
+                [
+                    'Origin' => ['*'], 
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], 
+                    'Access-Control-Request-Headers' => ['*'], 
+                    'Access-Control-Allow-Credentials' => null, 
+                    'Access-Control-Max-Age' => 86400, 
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Total-Count','X-Pagination-Page-Count', 'X-Pagination-Current-Page', 'X-Pagination-Per-Page'],
+                ]
             ]
         ];
 
