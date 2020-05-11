d086698664239ba314e53496d96d7bc95f07d46a
Jhonatan Bianchi <jhonatanbianchi@gmail.com>
2020-05-11 08:44:24 -0300

    Options

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -67,6 +67,7 @@ class BaseController extends ActiveController
 
         $behaviors['corsFilter'] = [
             'class' => Cors::className(),
+            ['Origin' => ['*'], 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], 'Access-Control-Request-Headers' => ['*'], 'Access-Control-Allow-Credentials' => true, 'Access-Control-Max-Age' => 86400, 'Access-Control-Expose-Headers' => []]
             // 'cors' => [
             //     [
             //         'Access-Control-Allow-Credentials' => true,
